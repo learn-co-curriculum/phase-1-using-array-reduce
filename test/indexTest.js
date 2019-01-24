ac6327600c41fce1fa78b8ec033cd7f3b68ebe89
Kellye Greene <kellyecodes@gmail.com>
2019-01-24 10:50:08 -0500

    Fixes tests

--- test/indexTest.js
+++ test/indexTest.js
@@ -3,15 +3,15 @@ const expect = chai.expect;
 describe('reducer', function() {
   describe('batteries', function() {
     it('should have a `totalBatteries` variable', function() {
-      expect(totalBatteries).toExist();
+      expect(totalBatteries).to.exist;
     });
 
     it('should have a number as a result', function() {
-      expect(totalBatteries).toBeA('number');
+      expect(totalBatteries).to.be.a('number');
     });
 
     it('should have made the sum of all the assembled batteries', function() {
-      expect(totalBatteries).toEqual(31);
+      expect(totalBatteries).to.eql(31);
     });
   });
 });
